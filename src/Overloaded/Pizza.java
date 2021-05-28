373db827fa5d01742df09c183eefb8d199d557f2
Acla Putra <aclalead@gmail.com>
2021-05-28 19:29:57 +0700

    added Overloaded Pizza

--- src/Overloaded/Pizza.java
+++ src/Overloaded/Pizza.java
@@ -1,2 +1,31 @@
-package Overloaded;public class Pizza {
+package Overloaded;
+
+public class Pizza {
+
+    String bread;
+    String sauce;
+    String cheese;
+    String topping;
+
+    Pizza() {
+        System.out.println("no pizza");
+    }
+    Pizza(String bread) {
+        this.bread = bread;
+    }
+    Pizza(String bread, String sauce) {
+        this.bread = bread;
+        this.sauce = sauce;
+    }
+    Pizza(String bread, String sauce, String cheese) {
+        this.bread = bread;
+        this.sauce = sauce;
+        this.cheese = cheese;
+    }
+    Pizza(String bread, String sauce, String cheese, String topping) {
+        this.bread = bread;
+        this.sauce = sauce;
+        this.cheese = cheese;
+        this.topping = topping;
+    }
 }
