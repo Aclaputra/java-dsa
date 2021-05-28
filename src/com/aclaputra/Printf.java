4e0b06da187d81dd11734b40db4d0772878c8a8d
Acla Putra <aclalead@gmail.com>
2021-05-28 15:30:18 +0700

    added Printf

--- src/com/aclaputra/Printf.java
+++ src/com/aclaputra/Printf.java
@@ -2,6 +2,50 @@ package com.aclaputra;
 
 public class Printf {
     public static void main(String[] args) {
-        System.out.printf("This is a format of String",123);
+
+        /**
+         * printf() = an optional method to control, format, and display text to the console window
+         *            two arguments = format string + (object/variable/value)
+         *            % [flags] [precision] [width] [conversion-character]
+         */
+        boolean myBoolean = true;
+        char myChar = '@';
+        String myString = "Acla";
+        int myInt = 50;
+        double myDouble = 1000;
+
+        /**
+         * [conversion-character]
+         */
+//        System.out.printf("%b ",myBoolean);
+//        System.out.printf("%c ",myChar);
+//        System.out.printf("%s ",myString);
+//        System.out.printf("%f",myDouble);
+        /**
+         * [width]
+         * minimum number of characters to be written as output
+         */
+//        System.out.printf("Hello %10s",myString);
+
+        /**
+         * [precision]
+         * sets number of digits of precision when outputting floating-point values
+         */
+//        System.out.printf("You have this much money %.1f",myDouble)
+//        System.out.printf("You have this much money %f ",myDouble);
+//        System.out.printf("You have this much money %.2f",myDouble);
+
+        /**
+         * [flags]
+         * adds an effect to ouput based on the flag added to format specifier
+         * - : left-justify
+         * + : output a plus ( + ) or minus ( - ) sign for a numeric value
+         * 0 : numeric values are zero-padded
+         * , : comma grouping separator if numbers > 1000
+         */
+//        System.out.printf("You have this much money %+f ",myDouble);
+//        System.out.printf("You have this much money %020f ",myDouble);
+//        System.out.printf("You have this much money %,f ",myDouble);
+
     }
 }
