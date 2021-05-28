bb6dcf0cb0ed0cd50a8a9c2fef47e649268bb209
Acla Putra <aclalead@gmail.com>
2021-05-28 19:29:16 +0700

    added VariableScope DiceRoller

--- src/VariableScope/DiceRoller.java
+++ src/VariableScope/DiceRoller.java
@@ -1,2 +1,17 @@
-package VariableScope;public class DiceRoller {
+package VariableScope;
+
+import java.util.Random;
+
+public class DiceRoller {
+    Random random;
+    int number = 0;
+
+    DiceRoller(){
+        random = new Random();
+        roll();
+    }
+    void roll() {
+        number = random.nextInt(6) + 1;
+        System.out.println(number);
+    }
 }
