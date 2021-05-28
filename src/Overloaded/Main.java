9a65c3e7ade641c40967c9d1863d18dabe51fda5
Acla Putra <aclalead@gmail.com>
2021-05-28 19:29:38 +0700

    added Overloaded Main

--- src/Overloaded/Main.java
+++ src/Overloaded/Main.java
@@ -1,2 +1,23 @@
-package Overloaded;public class Main {
+package Overloaded;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * overloaded constructors = multiple constructors within a class with the same name,
+         *                           but have different parameters
+         *                           name + parameters = signature.
+         */
+        Pizza pizza1 = new Pizza("thick crust","tomato","mozzarella","pepperoni");
+        Pizza pizza2 = new Pizza("thick crust","tomato","mozzarella");
+        Pizza pizza3 = new Pizza("thick crust","tomato");
+        Pizza pizza4 = new Pizza("thick crust");
+        Pizza pizza5 = new Pizza();
+
+        System.out.println("Here are the ingredients of your pizza: ");
+        System.out.println(pizza4.bread);
+        System.out.println(pizza3.sauce);
+        System.out.println(pizza2.cheese);
+        System.out.println(pizza1.topping);
+
+    }
 }
