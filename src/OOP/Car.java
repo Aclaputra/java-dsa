115487c62869c91f08b9adeb1f0dc8ee26f4b814
Acla Putra <aclalead@gmail.com>
2021-05-28 15:28:38 +0700

    added OOP for car

--- src/OOP/Car.java
+++ src/OOP/Car.java
@@ -1,2 +1,17 @@
-package OOP;public class Car {
+package OOP;
+
+public class Car {
+
+    String make = "Chevrolet";
+    String model = "Corvette";
+    int year = 2020;
+    String color = "blue";
+    double price = 50000.00;
+
+    void drive() {
+        System.out.println("You drive the car");
+    }
+    void brake() {
+        System.out.println("You step on the brakes");
+    }
 }
