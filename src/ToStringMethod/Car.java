17df599151231ff5227803c17468d1220c7ae06d
Acla Putra <aclalead@gmail.com>
2021-05-29 08:55:53 +0700

    added ToStringMethod Car

--- src/ToStringMethod/Car.java
+++ src/ToStringMethod/Car.java
@@ -1,2 +1,12 @@
-package ToStringMethod;public class Car {
+package ToStringMethod;
+
+public class Car {
+    String make = "Ford";
+    String model = "Mustang";
+    String color = "red";
+    int year = 2021;
+
+    public String toString() {
+        return make +"\n"+ model +"\n"+ color +"\n"+ year;
+    }
 }
