76223a6d825f194fec5a39c5fcbd9aec3c70d681
Acla Putra <aclalead@gmail.com>
2021-05-29 10:54:11 +0700

    added StaticKeyword Friend

--- src/StaticKeyword/Friend.java
+++ src/StaticKeyword/Friend.java
@@ -1,2 +1,15 @@
-package StaticKeyword;public class Friend {
+package StaticKeyword;
+
+public class Friend {
+    String name;
+    static int numberOfFriends;
+
+    Friend(String name){
+        this.name = name;
+        numberOfFriends++;
+    }
+
+    static void displayFriends(){
+        System.out.println("You have "+numberOfFriends+" Friends ");
+    }
 }
