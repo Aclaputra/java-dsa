b7d0411e43562d2792b3036ffde46a58cd15a919
Acla Putra <aclalead@gmail.com>
2021-05-29 10:55:00 +0700

    added objectPassing main

--- src/ObjectPassing/Main.java
+++ src/ObjectPassing/Main.java
@@ -1,2 +1,12 @@
-package ObjectPassing;public class Main {
+package ObjectPassing;
+
+public class Main {
+    public static void main(String[] args) {
+        Garage garage = new Garage();
+        Car car1 = new Car("Chevrolet");
+        Car car2 = new Car("Tesla");
+
+        garage.park(car1);
+        garage.park(car2);
+    }
 }
