7b98d1e1a3113adfe37e9a8f842651ba6476c587
Acla Putra <aclalead@gmail.com>
2021-05-29 08:55:20 +0700

    added ToStringMethod Main

---
+++ src/ToStringMethod/Main.java
@@ -0,0 +1,16 @@
+package ToStringMethod;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * toString() = special method that all objects inherit,
+         *              that returns a string that "textually represents" an object.
+         *              can be used both implicitly and explicitly.
+         */
+        Car car = new Car();
+
+        System.out.println(car);
+        System.out.println(car.toString());
+
+    }
+}
