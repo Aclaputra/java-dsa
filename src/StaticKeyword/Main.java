e1fa620697f52cc01448974c33d909254654d4d6
Acla Putra <aclalead@gmail.com>
2021-05-29 10:53:50 +0700

    added StaticKeyword Main

--- src/StaticKeyword/Main.java
+++ src/StaticKeyword/Main.java
@@ -1,2 +1,16 @@
-package StaticKeyword;public class Main {
+package StaticKeyword;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * static = modifier. A single copy of a variable/method is created and shared.
+         *          The class "owns" the static member.
+         */
+        Friend friend1 = new Friend("Rovan");
+        Friend friend2 = new Friend("Yovi");
+        Friend friend3 = new Friend("Ilham");
+        Friend friend4 = new Friend("Wishnu");
+
+        Friend.displayFriends();
+    }
 }
