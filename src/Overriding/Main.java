9276892bdcedb4377c2ad4f874f025017bf4ae63
Acla Putra <aclalead@gmail.com>
2021-05-29 16:46:51 +0700

    added Overriding main

--- src/Overriding/Main.java
+++ src/Overriding/Main.java
@@ -1,2 +1,16 @@
-package Overriding;public class Main {
+package Overriding;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * method overriding = Declaring a method in sub class,
+         *                     which is already present in parent class.
+         *                     done so that a child class can give its own implementation.
+         */
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+
+        animal.speak();
+        dog.speak();
+    }
 }
