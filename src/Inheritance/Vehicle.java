fae1b54235728a96d84134ad65ae784b9a8b5e7f
Acla Putra <aclalead@gmail.com>
2021-05-29 12:39:59 +0700

    modified Inheritance Vehicle

--- src/Inheritance/Vehicle.java
+++ src/Inheritance/Vehicle.java
@@ -1,2 +1,13 @@
-package Inheritance;public class Vehicle {
+package Inheritance;
+
+public class Vehicle {
+    double speed;
+
+    void go() {
+        System.out.println("This vehicle is moving");
+    }
+    void stop() {
+        System.out.println("This vehicle is stopped");
+    }
+
 }
