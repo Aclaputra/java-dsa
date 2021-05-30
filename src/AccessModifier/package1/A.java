abe4fce448c5dc71ab42f98f586596b920b1414e
Acla Putra <aclalead@gmail.com>
2021-05-31 06:46:57 +0700

    modified package1 A

--- src/AccessModifier/package1/A.java
+++ src/AccessModifier/package1/A.java
@@ -1,2 +1,10 @@
-package AccessModifier.package1;public class A {
+package AccessModifier.package1;
+
+import AccessModifier.package2.*;
+
+public class A {
+    public static void main(String[] args) {
+        C c = new C();
+        System.out.println(c.publicMessage);
+    }
 }
