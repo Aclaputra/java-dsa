c9dc66b32574979f8525da72de22313ab88a85dc
Acla Putra <aclalead@gmail.com>
2021-05-31 06:47:17 +0700

    modified package1 B

--- src/AccessModifier/package1/B.java
+++ src/AccessModifier/package1/B.java
@@ -1,2 +1,7 @@
-package AccessModifier.package1;public class B {
+package AccessModifier.package1;
+
+import AccessModifier.package2.*;
+
+public class B {
+    private String privateMessage = "This is the private";
 }
