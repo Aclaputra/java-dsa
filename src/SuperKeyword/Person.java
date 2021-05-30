a82193e2877be492510b8ccc70759c1c0ff6416c
Acla Putra <aclalead@gmail.com>
2021-05-30 07:46:16 +0700

    added SuperKeyword Person:

--- src/SuperKeyword/Person.java
+++ src/SuperKeyword/Person.java
@@ -1,2 +1,14 @@
-package SuperKeyword;public class Person {
+package SuperKeyword;
+
+public class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String toString() {
+        return this.name + "\n" + this.age + "\n";
+    }
 }
