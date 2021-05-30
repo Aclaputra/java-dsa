19fe82b60e87ad27befc424d76b6c29795e19795
Acla Putra <aclalead@gmail.com>
2021-05-30 07:47:19 +0700

    added SuperKeyword Hero

--- src/SuperKeyword/Hero.java
+++ src/SuperKeyword/Hero.java
@@ -1,2 +1,13 @@
-package SuperKeyword;public class Hero {
+package SuperKeyword;
+
+public class Hero extends Person {
+    String power;
+
+    Hero(String name,int age,String power){
+        super(name,age);
+        this.power = power;
+    }
+    public String toString(){
+        return super.toString()+this.power;
+    }
 }
