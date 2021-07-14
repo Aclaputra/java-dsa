dc6d4b2abee602ee0e001887abbd211ea1056daa
Acla Putra <aclaputra@github.com>
2021-07-15 01:16:17 +0700

    Added BigO Notation example

--- src/com/aclaputra/BigO/Main.java
+++ src/com/aclaputra/BigO/Main.java
@@ -1,2 +1,28 @@
-package com.aclaputra.BigO;public class Main {
+package com.aclaputra.BigO;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * O(n) linear time
+         * n = 1000000
+         * ~1000000 steps
+         */
+//        int addUp(int n) {
+//            for(int i = 0; i <= n; i++) {
+//                 sum += i;
+//            }
+//            return sum;
+//        }
+
+        /**
+         * O(1) constant time
+         * n = 1000000
+         * more simple
+         * ~3 steps
+         */
+//        int addUp(int n) {
+//            int sum = n * (n + 1) / 2;
+//            return sum;
+//        }
+    }
 }
